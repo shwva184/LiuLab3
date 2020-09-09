d5842cb7b917e9d5b3eb3f28ddcacd50ae6de9c4
shwva184 <vcshwetha2@gmail.com>
2020-09-09 15:11:02 +0200

    commit function documentation

--- lab3R/R/euclidean.R
+++ lab3R/R/euclidean.R
@@ -2,11 +2,14 @@
 #' 
 #' @param digit1 A number.
 #' @param digit2 A number.
+#' @description Implementation ofEuclidian algorithm to find the greatest common divisor of two numbers.
 #' @return The euclidean of \code{digit1} and \code{digit2}.
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
 #' @examples
 #' euclidean(1, 1)
 #' euclidean(10, 1)
 #' @export
+
 euclidean <-
 function(digit1,digit2){
   stopifnot(is.numeric(digit1),is.numeric(digit2),length(digit1)==1,length(digit2)==1)
