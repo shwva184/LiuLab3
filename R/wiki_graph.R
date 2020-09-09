5d393f61dabd1f372736b36ccfdcdccf303ad3d8
shwva184 <vcshwetha2@gmail.com>
2020-09-09 17:03:07 +0200

    commiting wiki_graph data

---
+++ R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' Data containinng nodes and weights of graph
+#'
+#' A dataset is containing the edges and weights of graph.
+#'
+#' @format A data frame with 54 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{edges of the graph-from}
+#'   \item{v2}{edges of the graph-to}
+#'   \item{w}{weight of the edge from v1 to v2}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)}
+"wiki_graph"
