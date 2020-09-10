aa5394bede1bd1c23ad0268a9acdc7241499cc2a
shwva184 <vcshwetha2@gmail.com>
2020-09-10 13:54:31 +0200

    Reverting the previous change

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -5,19 +5,18 @@
 #' @return The shortest distance from \code{init_node} to all other nodes in the graph.
 #' @references https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 #' @references https://en.wikipedia.org/wiki/Graph#Mathematics
-#' @import cppRouting
 #' @export
 
 dijkstra <- function(graph,init_node){
   stopifnot(is.data.frame(graph),length(graph)==3,names(graph[1])=="v1",names(graph[2])=="v2",names(graph[3])=="w", length(init_node)==1,is.numeric(init_node))
-  newgraph=makegraph(graph)
+  newgraph=cppRouting::makegraph(graph)
   visited=vector()
   j=1
   res=vector()
   to=unlist(graph[1])
   for (i in 1:length(to)){
     if((to[i]%in%visited)==FALSE){
-      res[j]=suppressMessages(get_distance_pair(newgraph,init_node,to[i],algorithm = "bi" ))
+      res[j]=suppressMessages(cppRouting::get_distance_pair(newgraph,init_node,to[i],algorithm = "bi" ))
       j=j+1
       visited=append(visited,to[i])
     }
